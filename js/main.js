ff485ff3be938c92dcdbba04ae6ab63f873ab9d5
Yan Campêlo <78034279+yanchagas04@users.noreply.github.com>
2022-06-27 18:37:00 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const button = document.querySelector("button");
 const showResult = document.querySelector("p");
 const selector = document.querySelector("select");
-
-const results = [6, 32, 10, 31, 21];
+const total_casos = 62;
+const quantidade_acidentes = [4, 20, 6, 19, 13];
+var results = [];
+for(var i = 0; i <= 5; i++){
+    results.push(Math.round((quantidade_acidentes[i]/total_casos) * 100));
+}
+//var results = [6, 32, 10, 31, 21];
 
 function clear() {
     showResult.textContent = "XX.XX%";
@@ -12,6 +17,6 @@ selector.addEventListener("focus", clear);
 
 button.addEventListener("click", () => {
     let option = document.getElementById("risks").value;
-    showResult.textContent = `${results[option]}.00%`;
+    showResult.textContent = `${results[option]}%`;
     drawChart();
 });
