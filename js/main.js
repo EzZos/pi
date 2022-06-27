865f32ff41df10754b2d74aaac387094c9866e4d
Yan Campêlo <78034279+yanchagas04@users.noreply.github.com>
2022-06-27 18:39:02 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const button = document.querySelector("button");
 const showResult = document.querySelector("p");
 const selector = document.querySelector("select");
+
 const total_casos = 62;
 const quantidade_acidentes = [4, 20, 6, 19, 13];
 var results = [];
+
 for(var i = 0; i <= 5; i++){
     results.push(Math.round((quantidade_acidentes[i]/total_casos) * 100));
 }
-//var results = [6, 32, 10, 31, 21];
 
 function clear() {
     showResult.textContent = "XX.XX%";
